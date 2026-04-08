2601c53cc0c2b7321981a1c1ad98ad180f17221e
Kelsen Gile <kelsengilesarmientoconel@gmail.com>
2026-04-08 18:13:15 +0800

    design only

--- Logic/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    DATABASE = os.path.join(BASE_DIR, '../data/armsdealer.db')
-    DEBUG = True
--- run.py
+++
@@ -1,6 +0,0 @@
-from logic.app import app
-from data.db import init_db
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
